2d47b619a1a5ade0716e40ab403de50f93acb516
yageek <yannick.heinrich@gmail.com>
2014-10-10 08:15:56 +0200

    Update compilation for win32

--- setup.py
+++ setup.py
@@ -3,15 +3,14 @@ from distutils.core import setup, Extension
 module1 = Extension('lambert',
 		define_macros = [('MAJOR_VERSION', '1'),
 			('MINOR_VERSION', '0')],
-		sources = ['lambertmodule.c', './lib/src/lambert.c',"./lib/src/lambert.h"])
+		sources = ['lambertmodule.c', './lib/src/lambert.c'])
 
 setup (name = 'lambert',
 		version = '1.0',
 		description = 'A Python wrapper to the C lambert library',
 		author = 'Yannick Heinrich',
 		author_email = 'yannick.heinrich@gmail.com',
-		url = 'http://docs.python.org/extending/building',
-		long_description = '''
-This is really just a demo package.
-''',
+		url = 'https://github.com/yageek/lambert-python',
+		keywords = ['lambert', 'coordinates', 'gps', 'wgs84'], # arbitrary keywords
+		headers = ["./lib/src/lambert.h"],
 ext_modules = [module1])
